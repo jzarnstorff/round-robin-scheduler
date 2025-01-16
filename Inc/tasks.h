e0372562be411d555d9542c4cbb4edd4b1635374
Jacob Zarnstorff <jacobzarnstorff@gmail.com>
2025-01-16 14:33:45 -0600

    Inc: tasks.h: Add header file for all the task handlers

---
+++ Inc/tasks.h
@@ -0,0 +1,20 @@
+/**
+ ********************************************************
+ * @file    Inc/tasks.h
+ * @author  Jacob Zarnstorff
+ * @date    09-January-2025
+ * @brief   This file contains function
+ *          prototypes for task handlers
+ ********************************************************
+ */
+
+#ifndef __TASKS_H__
+#define __TASKS_H__
+
+void idle_task(void);
+void task_0_handler(void);
+void task_1_handler(void);
+void task_2_handler(void);
+void task_3_handler(void);
+
+#endif // __TASKS_H__
