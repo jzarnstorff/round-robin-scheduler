172559046e8efcaf52e8a7a63067ace335bd8fa5
Jacob Zarnstorff <jacobzarnstorff@gmail.com>
2025-01-16 14:31:37 -0600

    Src: tasks.c: Add multiple task handlers to be added to the scheduler

---
+++ Src/tasks.c
@@ -0,0 +1,66 @@
+/**
+ ********************************************************
+ * @file    Src/tasks.c
+ * @author  Jacob Zarnstorff
+ * @date    09-January-2025
+ * @brief   This file contains function
+ *          definitions for task handlers
+ ********************************************************
+ */
+
+#include <stdio.h>
+
+/**
+ * @brief The idle task which is always marked as
+ *        `READY` and is never in a blocking state.
+ * @param None
+ * @retval None
+ */
+void idle_task(void) {
+    while (1)
+        ;
+}
+
+/**
+ * @brief A task to be scheduled that prints a message to the UART
+ * @param None
+ * @retval None
+ */
+void task_0_handler(void) {
+    while (1) {
+        printf("This is task 0\r\n");
+    }
+}
+
+/**
+ * @brief A task to be scheduled that prints a message to the UART
+ * @param None
+ * @retval None
+ */
+void task_1_handler(void) {
+    while (1) {
+        printf("This is task 1\r\n");
+    }
+}
+
+/**
+ * @brief A task to be scheduled that prints a message to the UART
+ * @param None
+ * @retval None
+ */
+void task_2_handler(void) {
+    while (1) {
+        printf("This is task 2\r\n");
+    }
+}
+
+/**
+ * @brief A task to be scheduled that prints a message to the UART
+ * @param None
+ * @retval None
+ */
+void task_3_handler(void) {
+    while (1) {
+        printf("This is task 3\r\n");
+    }
+}
