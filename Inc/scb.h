834cb897a55fcbb857c9095f7294fc8651be3af8
Jacob Zarnstorff <jacobzarnstorff@gmail.com>
2025-01-16 13:29:49 -0600

    Inc: scb.h: Add header file for system control block driver

---
+++ Inc/scb.h
@@ -0,0 +1,17 @@
+/**
+ ********************************************************
+ * @file    Inc/scb.h
+ * @author  Jacob Zarnstorff
+ * @date    09-January-2025
+ * @brief   This file provides function prototypes for
+ *          enabling processor faults in the system control
+ *          block of the Cortex M4 microcontroller.
+ ********************************************************
+ */
+
+#ifndef __SCB_H__
+#define __SCB_H__
+
+void enable_processor_faults(void);
+
+#endif // __SCB_H__
